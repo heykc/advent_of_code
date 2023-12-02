fa5bcbb216dbae8a43b97b6ce0b1dca64698154b
Keith Charles <keith.k.charles@gmail.com>
2023-12-02 09:51:25 -0700

    solve day 2 step 2

--- 2023/02/02.mjs
+++ 2023/02/02.mjs
@@ -37,9 +37,16 @@ const check_impossible_values = (line) => {
     : +id;
 };
 
+const pow_of_max_values = (line) => {
+  const { id, game } = parse_id(line);
+  const max_color_values = get_max_values(game);
+  return Object.values(max_color_values).reduce((acc, val) => acc * val, 1);
+}
+
 const parse_input = (input, fn) => {
   const lines = input.split('\n');
   return lines.reduce((acc, line) => acc + fn(line), 0);
 };
 
 console.log(parse_input(data, check_impossible_values));
+console.log(parse_input(data, pow_of_max_values));
