14d08431153739cc94b64defd1c5350fa6f35e2d
Keith Charles <keith.k.charles@gmail.com>
2023-12-02 10:02:21 -0700

    add documentation

--- 2023/02/02.mjs
+++ 2023/02/02.mjs
@@ -8,6 +8,12 @@ const color_maxes = {
 };
 const id_regex = new RegExp(/Game (\d+): /, 'd');
 
+/**
+ * Separate the ID from the game results string.
+ *
+ * @param {string} line - The whole line representing both the game id and rounds played.
+ * @returns {object} The id and the collection of rounds as 2 separate strings.
+ */
 const parse_id = (line) => {
   const { 1: id, indices: [ [ , after_match ] ] } = id_regex.exec(line);
   const game = line.substring(after_match);
@@ -15,6 +21,13 @@ const parse_id = (line) => {
   return { id, game };
 };
 
+/**
+ * Take each game and find the highest number of cubes pulled per color for the entire game of rounds.
+ * Rounds don't actually matter, so remove the semicolon to make a flat list of colors and their values.
+ *
+ * @param {string} game - the results of cubes pulled per round in a game.
+ * @returns {object} { red: ##, green: ##, blue: ## } where ## is the maximum value for each.
+ */
 const get_max_values = (game) => game
   .replaceAll(';', ',')
   .split`,`
@@ -24,6 +37,13 @@ const get_max_values = (game) => game
     return acc;
   }, {});
 
+/**
+ * Compare each maximum value of cubes pulled per color for a game and see
+ * if that value is greater than the color_maxes defined by the prompt.
+ *
+ * @param {string} line - A single game of cubes pulled split in rounds, prefixed with a game id.
+ * @returns {number} 0 if any color exceeds the max defined by the prompt, otherwise returns id number.
+ */
 const check_impossible_values = (line) => {
   const { id, game } = parse_id(line);
   const max_color_values = get_max_values(game);
@@ -36,12 +56,26 @@ const check_impossible_values = (line) => {
     : +id;
 };
 
+/**
+ * Multiplies all the highest numbers of cubes pulled per game together.
+ *
+ * @param {string} line - A single game of cubes pulled split in rounds, prefixed with a game id. 
+ * @returns {number} all the cube values multiplied together
+ */
 const pow_of_max_values = (line) => {
   const { id, game } = parse_id(line);
   const max_color_values = get_max_values(game);
   return Object.values(max_color_values).reduce((acc, val) => acc * val, 1);
 }
 
+/**
+ * Split the multiline input into separate lines, then add each line together based on
+ * the provided function.
+ *
+ * @param {string} input - The multiline input to add together.
+ * @param {Function} fn - The function to parse a line into a number.
+ * @returns {number} The total value of all lines added together.
+ */
 const parse_input = (input, fn) => {
   const lines = input.split('\n');
   return lines.reduce((acc, line) => acc + fn(line), 0);
